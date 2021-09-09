6302ffe6c30c97b3d19b7046b9ebfca35925382d
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-09-09 11:26:21 +0200

    RestController

--- src/main/java/com/PayMyBuddy/demo/comfiguretion/SpringSecurityConfig.java
+++ src/main/java/com/PayMyBuddy/demo/comfiguretion/SpringSecurityConfig.java
@@ -2,12 +2,17 @@ package com.PayMyBuddy.demo.comfiguretion;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.hibernate.criterion.Restrictions.and;
 
 @Configuration
 @EnableWebSecurity
@@ -16,20 +21,28 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("springuser").password("spring123").roles("USER")
+                .withUser("springuser").password(passwordEncoder().encode("spring123")).roles("USER")
                 .and()
-                .withUser("springadmin").password("admin123").roles("ADMIN", "USER");
+                .withUser("springadmin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
 
     }
 
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .oauth2Login();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
