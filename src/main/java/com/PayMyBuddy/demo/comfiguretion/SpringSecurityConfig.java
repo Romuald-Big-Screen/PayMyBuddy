acd780b1aa956bf08c9ced7d5286b5b56df7fc32
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-09-09 16:29:43 +0200

    nad

--- src/main/java/com/PayMyBuddy/demo/comfiguretion/SpringSecurityConfig.java
+++ src/main/java/com/PayMyBuddy/demo/comfiguretion/SpringSecurityConfig.java
@@ -1,49 +1,29 @@
 package com.PayMyBuddy.demo.comfiguretion;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static org.hibernate.criterion.Restrictions.and;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("springuser").password(passwordEncoder().encode("spring123"))
-                .roles("USER")
-                .and()
-                .withUser("springadmin").password(passwordEncoder().encode("admin123"))
-                .roles("ADMIN", "USER");
-
-    }
-
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("USER")
-                .anyRequest().authenticated()
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/login_form", "/register_form", "/css/", "/js/")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .formLogin();
-
+                .formLogin()
+                .loginPage("/index.html")
+                .defaultSuccessUrl("/*", true)
+                .and()
+                .httpBasic()
+                .and()
+                .rememberMe();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
 }
