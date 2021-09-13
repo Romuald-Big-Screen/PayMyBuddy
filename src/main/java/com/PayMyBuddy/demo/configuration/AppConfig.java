d11553ca65551748c4f8ad253afa6f62a7c9454a
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-09-13 16:44:44 +0200

    RestController open2

--- src/main/java/com/PayMyBuddy/demo/configuration/AppConfig.java
+++ src/main/java/com/PayMyBuddy/demo/configuration/AppConfig.java
@@ -1,4 +1,34 @@
 package com.PayMyBuddy.demo.configuration;
 
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/Register.css","/index")
+                .permitAll()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/index")
+                .defaultSuccessUrl("/",true)
+                .and()
+                .httpBasic();
+
+
+
+    }
 }
--- src/main/java/com/PayMyBuddy/demo/model/User.java
+++ src/main/java/com/PayMyBuddy/demo/model/User.java
@@ -2,7 +2,7 @@ package com.PayMyBuddy.demo.model;
 
 
 import lombok.Data;
-import javax.persistence.Id;
+
 import javax.persistence.*;
 
 @Data
@@ -13,11 +13,15 @@ public class User {
     @Id 
 
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
-
-    private int id;
+@SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ")
+    private Integer id;
     private String firstName;
     private String lastName;
-
+@Column(unique = true)
+private String email;
+private String password;
+@ManyToMany
+private List<Connection> connections;
     public User(int id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
