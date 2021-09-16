72bebb2c83c7a633e14b5e95e70297048cf3d785
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-09-16 16:36:00 +0200

    ?

--- src/main/java/com/PayMyBuddy/demo/comfiguretion/SpringSecurityConfig.java
+++ src/main/java/com/PayMyBuddy/demo/comfiguretion/SpringSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.PayMyBuddy.demo.comfiguretion;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -25,5 +27,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .rememberMe();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
     }
 }
