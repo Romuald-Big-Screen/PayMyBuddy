3b747f1e94e08c27b3b664afe9392c78700b3560
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-09-17 10:43:11 +0200

    RestController open 2

--- src/main/java/com/PayMyBuddy/demo/mapper/UserMapper.java
+++ src/main/java/com/PayMyBuddy/demo/mapper/UserMapper.java
@@ -4,4 +4,7 @@ import org.springframework.security.core.userdetails.User;
 
 @Mapper
 public interface UserMapper extends AbstractMapper<User, UserDto>{
+    @Override
+    User toEntity(UserDto dto);
+
 }
